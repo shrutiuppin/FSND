e118637d4dc2d3d0b8136bc5f90f84590844987e
Shruti <shruti.uppin@target.com>
2021-02-27 20:40:21 +0530

    Update setup.sh

--- projects/capstone/starter/setup.sh
+++ projects/capstone/starter/setup.sh
@@ -1,1 +1,3 @@
-
+export DATABASE_URL='postgres://pcyueqjniubtbw:b8864840b5703425b4a614a3af4a49102a38f88e5bda7710863b0f9ebf87dac0@ec2-54-242-43-231.compute-1.amazonaws.com:5432/db50jffof1vuak'
+export FLASK_APP=app
+export FLASK_DEBUG=True
